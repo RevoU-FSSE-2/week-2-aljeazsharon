bcf27df1454910270bb91d7441b9ec7cb6b2e2d6
Sharon Aljeazsa L Silalahi <aljeazsharon@gmail.com>
2023-06-23 00:18:25 +0700

    added index.js responsive.css style.css

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-console.log(latihan-assignment);
-
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  var x = document.getElementById("menu-nav");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
